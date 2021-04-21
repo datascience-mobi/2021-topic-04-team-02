0a760cd64ac77921fc6b87dd56755ac9810d6e24
j0j0-99 <69297892+j0j0-99@users.noreply.github.com>
2021-04-22 00:23:15 +0200

    test

--- digit_recognition_test.py
+++ digit_recognition_test.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy
+import pandas as pd
 
-image = numpy.loadtxt('/Users/jojo3/PycharmProjects/pythonstuff/data/mnist_train.csv',  delimiter=',')
-image = image[:,1:]
-X = image[6,:].reshape(28,28)
-print(image.shape)
-figure()
-plt.imshow(X)
+#laden der .csv in ein Array
+images = numpy.loadtxt('/Users/jojo3/PycharmProjects/pythonstuff/data/mnist_train.csv',  delimiter=',')
+#image = image[:,1:] #erste Spalte ist die "Legende" für die im Bild codierte Zahl
+#X = image[6,:].reshape(28,28) #X = Bild in der 6. zeile der.csv
+print(images.shape)
+
+dataset = pd.DataFrame(images)
+dataset.head()
 
 
 
