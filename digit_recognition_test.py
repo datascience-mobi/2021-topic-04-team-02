8a4fe09e8ab3d15f6f4648ea25efba8e1c5957d2
j0j0-99 <69297892+j0j0-99@users.noreply.github.com>
2021-04-22 17:17:34 +0200

    testing stuff

--- digit_recognition_test.py
+++ digit_recognition_test.py
@@ -1,15 +1,18 @@
-import matplotlib.pyplot as plt
-import numpy
-import pandas as pd
 
+#import pandas as pd
 #laden der .csv in ein Array
-images = numpy.loadtxt('/Users/jojo3/PycharmProjects/pythonstuff/data/mnist_train.csv',  delimiter=',')
+#images = numpy.loadtxt('/data/mnist_train.csv',  delimiter=',')
 #image = image[:,1:] #erste Spalte ist die "Legende" für die im Bild codierte Zahl
 #X = image[6,:].reshape(28,28) #X = Bild in der 6. zeile der.csv
-print(images.shape)
-
-dataset = pd.DataFrame(images)
-dataset.head()
-
-
+#print(images.shape)
+#dataset = pd.DataFrame(images)
+#dataset.head()
+import numpy as np
+import pickle
+X = np.genfromtxt('data/mnist_train.csv', delimiter=',',skip_header=1).astype(np.dtype('uint8'))
+with open('data/train_points.p', 'wb') as f:
+    pickle.dump(X, f)
+...
+with open('data/train_points.p', 'rb') as f:
+    X = pickle.load(f)
 
