9eeb62b5751f9933473688794963c65ca986bcd5
j0j0-99 <69297892+j0j0-99@users.noreply.github.com>
2021-04-23 00:06:51 +0200

    hit or miss, I guess they never miss huh

--- digit_recognition_test.py
+++ digit_recognition_test.py
@@ -2,40 +2,53 @@ import numpy as np
 import pickle
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score
-
+from scipy.stats import mode
+def I_turned_myself_into_a_pickle_morty(data,new_data_location)
+    X = np.genfromtxt(data, delimiter=',').astype(np.dtype('uint8'))
+    with open(new_data_location, 'wb') as f:
+        pickle.dump(X, f)
+    ...
+    with open(new_data_location, 'rb') as f:
+        X = pickle.load(f)
 def do_pca(n_components, data): #does pca + data standardization I think?
     X = StandardScaler().fit_transform(data)
     pca = PCA(n_components)
     X_pca = pca.fit_transform(X)
     return pca, X_pca
-
-with open('data/train_points.p', 'rb') as f:
-    X = pickle.load(f)
-trainlabels = X[:,0]
-trainvalues = X[:,1:]
-
-pca, trainvalues_pca = do_pca(59,trainvalues)
-
-with open('data/test_points.p', 'rb') as f:
-    Y = pickle.load(f)
-testvalues = Y[:,1:]
-testlabels = Y[:,0]
-testvalues.shape
-
-pca, testvalues_pca = do_pca(59,testvalues)
-
-from scipy.stats import mode
-k=100
+def load_the_training_pickle(pickleddata):
+    with open(pickleddata, 'rb') as f:
+        X = pickle.load(f)
+    global trainlabels
+    trainlabels = X[:,0]
+    global trainvalues
+    trainvalues = X[:,1:]
+def load_the_testing_pickle(pickleddata):
+    with open(pickleddata, 'rb') as f:
+        Y = pickle.load(f)
+    global testvalues
+    testvalues = Y[:,1:]
+    global testlabels
+    testlabels = Y[:,0]
 def predict(x):
     differences = (trainvalues_pca - x)
     distances = np.einsum('ij, ij->i', differences, differences)
     nearest = trainlabels[np.argsort(distances)[:k]]
     return mode(nearest)[0][0]
+
+#K + Hauptkomponenten festlegen:
+number_of_pcs = 59
+k=1000
 hit = 0
 miss = 0
+
+load_the_training_pickle('data/train_points.p')
+load_the_testing_pickle('data/test_points.p')
+
+pca, testvalues_pca = do_pca(number_of_pcs,testvalues)
+pca, trainvalues_pca = do_pca(number_of_pcs,trainvalues)
+
+
+
 for i in range(0,9999): #PCA helps a lot
     sample = i
     predicted_value = predict(x=testvalues_pca[sample,:])
