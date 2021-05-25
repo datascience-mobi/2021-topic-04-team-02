876a8f777dc2af965f6223a5dcd5a663ba125f57
ngutzeit <ninagutzeit@gmail.com>
2021-05-25 15:30:19 +0200

    imported numpy and started code for pca

---
+++ PCA.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+# code for standard scaler of the data
+from sklearn.preprocessing import StandardScaler
+X_scaled = StandardScaler().fit_transform(X)
+X_scaled[:5]
+
+# calculating covariance matrix
+features = X_scaled.T
+cov_matrix = np.cov(features)
+cov_matrix[:5]
