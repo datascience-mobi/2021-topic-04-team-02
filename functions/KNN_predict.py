fd7c71ab93c790eaa546265be709f14b6761db96
j0j0-99 <69297892+j0j0-99@users.noreply.github.com>
2021-06-09 11:31:02 +0200

    update knn and project report

--- functions/KNN_predict.py
+++ functions/KNN_predict.py
@@ -2,7 +2,16 @@ import numpy as np
 from numpy.linalg import norm
 from scipy.stats import mode
 
+
 def knn(trainvalues_pca, trainlabels, X, k):
+    """
+
+    :param trainvalues_pca:
+    :param trainlabels:
+    :param X:
+    :param k:
+    :return:
+    """
     distances = np.linalg.norm(trainvalues_pca - X, axis=1) #calculates the euclidean distance between trainvalues and data point X
     nearest = trainlabels[np.argsort(distances)[:k]] #extracting the k nearest neighbours from the trainlabels through sorting the distances
     return mode(nearest)[0][0] #returning the major vote of the k nearest neighbours
