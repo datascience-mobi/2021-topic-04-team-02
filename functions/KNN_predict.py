57ce0397aef04e05a71b099d2cc8725df1e40b16
j0j0-99 <69297892+j0j0-99@users.noreply.github.com>
2021-06-15 18:42:21 +0200

    update knn

--- functions/KNN_predict.py
+++ functions/KNN_predict.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.stats import mode
 
 
-def knn(trainvalues_pca, trainlabels, X, k):
+def knn_euclidean(trainvalues_pca, trainlabels, X, k):
     """
 
     :param trainvalues_pca:
@@ -15,22 +15,21 @@ def knn(trainvalues_pca, trainlabels, X, k):
     nearest = trainlabels[np.argsort(distances)[:k]] #extracting the k nearest neighbours from the trainlabels through sorting the distances
     return mode(nearest)[0][0]
 
-
-def knn_manhattan(trainvalues_pca, trainlabels, testvalues_pca, k):
+def knn_manhattan(trainvalues_pca, trainlabels, X, k):
     """
 
     :param trainvalues_pca:
     :param trainlabels:
     :param X:
     :param k:
-    :return:
+    :return: returns the major vote of the k nearest neighbours
     """
-    distances = np.abs(trainvalues_pca[np.newaxis] - testvalues_pca[:, np.newaxis]).sum(axis=-1)
-    nearest = trainlabels[np.argsort(distances)[:,:k]]
-
+    distances = np.abs(trainvalues_pca - X).sum(-1)
+    nearest = trainlabels[np.argsort(distances)[:k]] #extracting the k nearest neighbours from the trainlabels through sorting the distances
     return mode(nearest)[0][0]
 
-def knn_euclidean(trainvalues_pca, trainlabels, testvalues_pca, k):
+
+def knn_testing(trainvalues_pca, trainlabels, testvalues_pca,k):
     """
 
     :param trainvalues_pca:
@@ -39,7 +38,8 @@ def knn_euclidean(trainvalues_pca, trainlabels, testvalues_pca, k):
     :param k:
     :return:x
     """
-    distances = np.sqrt(((trainvalues_pca[np.newaxis] - testvalues_pca[:, np.newaxis])**2).sum(axis=-1))
-    nearest = trainlabels[np.argsort(distances)[:,:k]]
 
-    return mode(nearest)[0][0]
+    distances = np.sqrt(((trainvalues_pca[np.newaxis] - testvalues_pca[:, np.newaxis])**2).sum(axis=-1))  # calculates the euclidean distance between trainvalues and data point X
+    nearest = trainlabels[np.argsort(distances)[:,:k]]  # extracting the k nearest neighbours from the trainlabels through sorting the distances
+    return nearest
+
