7e8aef4822083a011699e0974d85749beb8cd415
ngutzeit <ninagutzeit@gmail.com>
2021-06-16 09:13:51 +0200

    added vis_pca

---
+++ visualize/vis_pca.py
@@ -0,0 +1,11 @@
+# visualize pca data
+import plotly.express as px
+from final import *
+
+fig = px.scatter_3d(test_values_pca, x=0, y=1, z=2, color=test_labels)
+fig.update_traces(marker=dict(size=3))
+fig1 = px.scatter(train_values_pca, x=0, y=1, color=train_labels)
+fig2 = px.scatter(test_values_pca, x=0, y=1, color=test_labels)
+fig.show()
+fig1.show()
+fig2.show()
