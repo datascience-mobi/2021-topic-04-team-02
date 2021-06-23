69a2fbf56e63ec3cbaeae0f7bde1bc902dce2ac6
mahimier <42866471+mahimier@users.noreply.github.com>
2021-06-23 09:37:20 +0200

    added doc strings and meeting notes

--- functions/PCA.py
+++ functions/PCA.py
@@ -2,6 +2,14 @@ import numpy as np
 
 
 def PCA_func(stan_x, X_mean, num_components, train_evs=None):
+    """
+
+    :param stan_x: standardized test values
+    :param X_mean: mean of test values
+    :param num_components: number of components of test
+    :param train_evs: eigenvectors of train points
+    :return: returns each test point after dimensionality reduction
+    """
     # eigen values
     eigen_values, eigen_vectors = np.linalg.eigh(stan_x)
     # eigenvectors
