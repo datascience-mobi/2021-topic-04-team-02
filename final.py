5461e76be0f00af674bfda11d3b5f05dba12f8fd
Emmak02 <82159105+Emmak02@users.noreply.github.com>
2021-06-24 15:54:07 +0200

    update

--- final.py
+++ final.py
@@ -15,10 +15,10 @@ train_labels, train_values = load_the_pickle('data/train_points.p')
 test_labels, test_values = load_the_pickle('data/test_points.p')
 
 # standardization and PCA:
-train_values_centered, train_mean = center(train_values)
+train_values_centered, train_mean = center(train_values, Y="None")
 train_values_pca, train_evs = PCA_func(train_values_centered, train_mean, number_of_pcs)
 
-test_values_centered, test_mean = center_test_values(test_values, train_values)
+test_values_centered, test_mean = center(test_values, Y=train_values)
 test_values_pca, _ = PCA_func(test_values_centered,test_mean, number_of_pcs, train_evs=train_evs)
 
 def knn_neu(x):
--- functions/Standardize.py
+++ functions/Standardize.py
@@ -1,6 +1,85 @@
 import numpy as np
 
 
+def cov_mod(m, n, rowvar=True):
+    """
+    Estimate a covariance matrix, given data.
+    Covariance indicates the level to which two variables vary together.
+    If we examine N-dimensional samples, :math:`X = [x_1, x_2, ... x_N]^T`,
+    then the covariance matrix element :math:`C_{ij}` is the covariance of
+    :math:`x_i` and :math:`x_j`. The element :math:`C_{ii}` is the variance
+    of :math:`x_i`.
+    Parameters
+    ----------
+    m : array_like
+        A 1-D or 2-D array containing multiple variables and observations.
+        Each row of `m` represents a variable, and each column a single
+        observation of all those variables. Also see `rowvar` below.
+    n : array_like
+        An array containing multiple variables and observations.
+    rowvar : bool, optional
+        If `rowvar` is True (default), then each row represents a
+        variable, with observations in the columns. Otherwise, the relationship
+        is transposed: each column represents a variable, while the rows
+        contain observations.
+    Returns
+    -------
+    out : ndarray
+        The covariance matrix of the variables.
+    """
+
+    dtype = np.result_type(m, np.float64)
+
+    X = np.array(m, ndmin=2, dtype=dtype)
+    if not rowvar and X.shape[0] != 1:
+        X = X.T
+    if X.shape[0] == 0:
+        return np.array([]).reshape(0, 0)
+    dtype2 = np.result_type(n, np.float64)
+    X2 = np.array(n, ndmin=2, dtype=dtype2)
+    if not rowvar and X2.shape[0] != 1:
+        X2 = X2.T
+    if X2.shape[0] == 0:
+        return np.array([]).reshape(0, 0)
+
+    # Averages to be subtracted taken from second (training) array
+    avg = np.average(X2, axis=1)
+
+    # N-1 calculated:
+    N_1 = X.shape[1] - 1
+
+    # Averages subtracted:
+    X -= avg[:, None]
+
+    # [...]
+    X_T = X.T
+    c = np.dot(X, X_T.conj())
+
+    # Multiplied by 1/(N-1)
+    c *= np.true_divide(1, N_1)
+    # output squeeze [...]
+    return c.squeeze()
+
+
+def center(X, Y, scale=False):
+    if Y == "None":
+        mean = np.mean(X, axis=0)
+        Y = X
+    else:
+        mean = np.mean(Y, axis=0)
+    X_mean = X - mean
+    cov_mat = cov_mod(X, Y, rowvar=False)
+    c_mat = cov_mat
+    if scale:
+        # WIP
+        corr_mat = np.corrcoef(X_mean, rowvar=False)
+        corr_mat[np.isnan(corr_mat)] = cov_mat[np.isnan(corr_mat)]
+        c_mat = corr_mat
+    return c_mat, X_mean
+
+
+# Sad, inefficient and unused code below:
+
 def corr_columns(mat):
     x = len(mat[0])
     c = (1 / x)
@@ -31,30 +110,10 @@ def cov_columns(mat, mat2):
     return cov_mat
 
 
-def center(X, scale=False):
-    """
-
-    :param X: data point out of test set
-    :param scale:
-    :return: returns either covariance matrix or correlations matrix
-    """
-    mean = np.mean(X, axis=0)
-    X_mean = X - mean
-    cov_mat = np.cov(X_mean, rowvar=0)
-    # cov_mat = cov_columns(X_mean, X_mean)
-    c_mat = cov_mat
-    if scale:
-        corr_mat = np.corrcoef(X_mean, rowvar=False)
-        corr_mat[np.isnan(corr_mat)] = cov_mat[np.isnan(corr_mat)]
-        # or replace with 0???
-        c_mat = corr_mat
-    return c_mat, X_mean
-
-
 def center_test_values(X, Y, scale=False):
     mean = np.mean(Y, axis=0)
     X_mean = X - mean
-    cov_mat = np.cov(X_mean, rowvar=0)
+    cov_mat = np.cov(X_mean, rowvar=False)
     # cov_mat = cov_columns(X_mean, Y)
     c_mat = cov_mat
     if scale:
