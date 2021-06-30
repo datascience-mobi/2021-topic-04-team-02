481211bbb057fc50b89dc66b6f756b848f549945
j0j0-99 <69297892+j0j0-99@users.noreply.github.com>
2021-06-30 10:07:44 +0200

    update project report and details in knn_predict.py and final.py

--- final.py
+++ final.py
@@ -21,8 +21,8 @@ train_values_pca, train_evs = PCA_func(train_values_centered, train_mean, number
 test_values_centered, test_mean = center(test_values, Y=train_values)
 test_values_pca, _ = PCA_func(test_values_centered,test_mean, number_of_pcs, train_evs=train_evs)
 
-def knn_neu(x):
-    return knn(distance_method="euclidean",trainvalues_pca=train_values_pca, X=test_values_pca[x,:], trainlabels=train_labels, k=k)
+def knn_multi(x):
+    return knn(distance_method_as_string="euclidean",trainvalues_pca=train_values_pca, X=test_values_pca[x,:], trainlabels=train_labels, k=k)
 # kNN:
 hit = 0
 miss = 0
@@ -32,7 +32,7 @@ import multiprocessing
 l = list(range(10000))
 if __name__ == '__main__':
     with multiprocessing.Pool(4) as p:
-        result = p.map(knn_neu, range(10000))
+        result = p.map(knn_multi, range(10000))
     for sample in range(10000):
         if result[sample] == test_labels[sample]:
             hit += 1
