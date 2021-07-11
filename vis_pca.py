979702541fcd4d9c846c35725c4f3dfdf22728b2
mahimier <42866471+mahimier@users.noreply.github.com>
2021-07-11 17:49:50 +0200

    updated report, added PCA text

--- vis_pca.py
+++ vis_pca.py
@@ -13,7 +13,7 @@ test_values_centered, test_mean = center(test_values, train_values)
 test_values_pca, _ = PCA_func(test_values_centered,test_mean, number_of_pcs, train_evs=train_evs)
 
 
-components_after = px.scatter_3d(test_values_pca, x=0, y=1, z=2, color=test_labels)
-components_after.update_traces(marker=dict(size=3))
+components_test3 = px.scatter_3d(test_values_pca, x=0, y=1, z=2, color=test_labels)
+components_test3.update_traces(marker=dict(size=3))
 components_test = px.scatter(train_values_pca, x=0, y=1, color=train_labels)
 components_train = px.scatter(test_values_pca, x=0, y=1, color=test_labels)
