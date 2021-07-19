0ad36154f1ad351a7943f218350013b4d0fd2913
j0j0-99 <69297892+j0j0-99@users.noreply.github.com>
2021-07-19 19:05:41 +0200

    updating report + visualize pca

--- functions/visualize_pca.py
+++ functions/visualize_pca.py
@@ -4,29 +4,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def components_3d(test_values_pca, test_labels):
-    """
-    generates 3D plot using the values after pca colored depending on their label
-    :param test_values_pca: input values
-    :param test_labels: input labels determining the color
-    :return: returns 3D plot
-    """
-    plot3d = px.scatter_3d(test_values_pca, x=0, y=1, z=2, color=test_labels)
-    plot3d.update_traces(marker=dict(size=3))
+#def components_3d(test_values_pca, test_labels):
+#    """
+#    generates 3D plot using the values after pca colored depending on their label
+#    :param test_values_pca: input values
+#    :param test_labels: input labels determining the color
+#    :return: returns 3D plot
+#    """
+#    plot3d = px.scatter_3d(test_values_pca, x=dimension 1, y=dimension 2, z=dimension 3, color=test_labels)
+#    plot3d.update_traces(marker=dict(size=3))
+#    plot3d.update_xaxes(title_text="Dimension 1")
 
-    return plot3d.show()
+#    return plot3d.show()
 
 
-def components_2d(values_pca, labels):
-    """
-    generates 2D plot using the values after pca colored depending on their label
-    :param values_pca: input values
-    :param labels: input labels determining the color
-    :return: returns 2D plot
-    """
-    plot2d = px.scatter(values_pca, x=0, y=1, color=labels)
+def components_3d(test_values_pca,labels,figure_title):
+    mydata = test_values_pca
+    x = mydata[:, 0]
+    y = mydata[:, 1]
+    z = mydata[:, 3]
 
-    return plot2d.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    fig.add_axes(ax)
+    plt.scatter(x, y, z,c=labels)
+    ax.set_xlabel("PC 1")
+    ax.set_ylabel("PC 2")
+    ax.set_zlabel("PC 3")
+    ax.set_title(figure_title)
+    fig = plt.show()
+
+    return fig
+
+def components_2d(test_values_pca,labels, title, x_label, y_label):
+    mydata = test_values_pca
+    x = mydata[:, 0]
+    y = mydata[:, 1]
+
+    plt.scatter(x, y, c=labels)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    fig = plt.show()
+
+    return fig
 
 
 def components_reduced(data, train_evs):
